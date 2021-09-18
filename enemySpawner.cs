b2a69fb5bbca6a179fe2967e17a2381278890cdc
anni55 <90946735+anni55@users.noreply.github.com>
2021-09-18 22:03:45 +0530

    Add files via upload

---
+++ enemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemySpawner : MonoBehaviour 
+{
+	public GameObject enemy;
+	float randX;
+	Vector3 whereToSpawn;
+	public float spawnRate;
+	float nextSpawn = 0.0f;
+
+	void Start()
+	{
+		
+	}
+
+	void Update () 
+	{
+		if(Time.time > nextSpawn)
+		{
+			nextSpawn = Time.time + spawnRate;
+			randX = Random.Range(-20f, 20f);
+			whereToSpawn = new Vector3 (randX, transform.position.y, transform.position.z);
+			Instantiate (enemy, whereToSpawn, Quaternion.identity);
+		}
+	}
+	
+	
+	public void AdjustSpeed(float newspeed)
+	{
+		spawnRate = newspeed;
+	}
+}
---
+++ move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour 
+{
+	public float movespeed;
+	public Rigidbody rb;
+	float dirX;
+	
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		dirX = Input.acceleration.x * movespeed;
+		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, -20.3f, 20.3f),transform.position.y, transform.position.z);
+		transform.Rotate(0,50*Time.deltaTime,0);
+
+
+		rb.velocity = new Vector3 (dirX, 0f, 0f);
+	}
+}
